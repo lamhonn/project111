c4168da866b18fa7e535c3f853fe8b637c0e1db2
Lam Honn <lamhonn@gmail.com>
2025-12-04 17:09:05 +0200

    Revert "front code added fully. I'm adding other pngs later. Run first npm install -> npm run dev (#3)"
    This reverts commit 48a1bcb4ec7220b3ed429f941a6e3ce9b436acab.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,176 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { OrderProvider } from "./contexts/OrderContext";
-import MainForm from "./pages/MainForm";
-import MenuItems from "./pages/MenuItems";
-import NotFound from "./pages/NotFound";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const queryClient = new QueryClient();
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <OrderProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<MainForm />} />
-            <Route path="/category/:category" element={<MenuItems />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </OrderProvider>
-  </QueryClientProvider>
-);
+//TODO: move these graphql functions to a different file when layout requires it
+function testGraphqlAPI() {
+  const query = `
+    query {
+      hello
+    }
+  `;
 
+  graphqlClient.query(query).then((data) => {
+    console.log('GraphQL API response:', data);
+  }).catch((error) => {
+    console.error('Error calling GraphQL API:', error);
+  });
+}
+
+async function queryItems() {
+  const query = `
+    query GetItems {
+      items {
+        id
+        name
+        description
+        price
+        categoryID
+        created_at
+        updated_at
+      }
+    }
+  `;
+
+  try {
+    const queryResponse = await graphqlClient.query(query);
+    console.log('Response: ', queryResponse);
+
+    return queryResponse.data.items;
+
+  } catch(error) {
+    console.error('Failed to fetch items: ', error);
+  }
+}
+//TODO: create input types for all mutations for client-side validations
+async function testCreateItem() {
+  const item = {
+    name: "Paras Hanppari",
+    description: "Kaupungin paras hanppari on nyt täällä!",
+    price: 10.00,
+    categoryID: 3
+  };
+
+  const query = `
+    mutation CreateItem($input: CreateItem!) {
+      createItem(input: $input) {
+        code
+        success
+        message
+        item {
+          id
+          name
+          description
+          price
+          categoryID
+          created_at
+          updated_at
+        }
+      }
+    }
+  `;
+
+  try {
+    const newItem = await graphqlClient.query(query, { input: item });
+    console.log('Created item:', newItem);
+  } catch (error) {
+    console.error('Error creating item:', error);
+  }
+}
+
+//TODO: Do we need to check if item has changed before updating?
+//TODO: Do we need to handle race conditions when multiple updates occur simultaneously?
+
+async function testUpdateItem() {
+  const itemUpdate = {
+    id: 20,
+    name: "Updated Hanppari 3",
+    description: "Päivitetty kuvaus 3",
+    price: 14.50,
+    categoryID: 2
+  };
+
+  const query = `
+    mutation UpdateItem($input: UpdateItem!) {
+      updateItem(input: $input) {
+        code
+        success
+        message
+        item {
+          id
+          name
+          description
+          price
+          categoryID
+          created_at
+          updated_at
+        }
+      }
+    }
+  `;
+
+  try {
+    const updatedItem = await graphqlClient.query(query, { input: itemUpdate });
+    console.log('Updated item:', updatedItem);
+  } catch (error) {
+    console.error('Error updating item:', error);
+  }
+};
+
+
+async function testDeleteItem() {
+  const itemId = 23;
+
+  const query = `
+    mutation DeleteItem($id: ID!) {
+      deleteItem(id: $id) {
+        code
+        success
+        message
+      }
+    }
+  `;
+
+   try {
+    const deletedItem = await graphqlClient.query(query, { id:  itemId});
+    console.log('Deleted item successfully');
+  } catch (error) {
+    //TODO: security wise we don't want to disclose if ID exists in our database, so we shouldn't return an error here. Instead we want to only log it server side.
+    console.error('Error deleting item:', error);
+  }  
+}
+// Simple test without async function
+queryItems()
+  .then(items => {
+    console.log('Fetched items:', items);
+  })
+  .catch(error => {
+    console.error('Error fetching items:', error);
+  });
+
+testGraphqlAPI();
+testCreateItem();
+testUpdateItem();
+testDeleteItem();
 export default App;
